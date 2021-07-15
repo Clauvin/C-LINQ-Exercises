65b1110fe78b6b58c7bb2bb5973bcee2bc71f4e3
Cláuvin <Clauvin@users.noreply.github.com>
2021-07-15 16:45:34 -0300

    Update Exercise 1 - Basic LINQ.cs

--- Exercise 1 - Basic LINQ.cs
+++ Exercise 1 - Basic LINQ.cs
@@ -5,20 +5,20 @@ public class Program
 	public static void Main()
 	{
 		 // The Three Parts of a LINQ Query:
-        // 1. Data source.
-        int[] numbers = new int[16] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+		// 1. Data source.
+		int[] numbers = new int[16] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-        // 2. Query creation.
-        // numQuery is an IEnumerable<int>
-        var numQuery =
-            from num in numbers
-            where num < 10 
-            select num;
+		// 2. Query creation.
+		// numQuery is an IEnumerable<int>
+		var numQuery =
+		    from num in numbers
+		    where num < 10 
+		    select num;
 
-        // 3. Query execution.
-        foreach (int num in numQuery)
-        {
-            Console.Write(num + " ");
-        }
+		// 3. Query execution.
+		foreach (int num in numQuery)
+		{
+		    Console.Write(num + " ");
+		}
 	}
 }
